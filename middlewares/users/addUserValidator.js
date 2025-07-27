cfde2075de42dfe43a50622312a7dc1ff60d9060
wakiullah <m.wakiullah1212@gmail.com>
2025-07-27 22:43:20 +0600

    handle is email alredy exist error

--- middlewares/users/addUserValidator.js
+++ middlewares/users/addUserValidator.js
@@ -1,8 +1,17 @@
 const { check } = require("express-validator");
+const People = require("../../models/people");
+const createError = require('http-errors')
 
 const addUserValidator = [check('name').isLength({ min: 1 }).withMessage('Name is required').isAlpha('en-US', { ignore: ' -' }).withMessage('Enter name with only alpha').trim(),
 check('email').isEmail().withMessage('Please Enter a valid email address!').trim().custom(async (value) => {
-
+    try {
+        const data = await People.findOne({ email: value })
+        if (data) {
+            throw createError('User Already Exist')
+        }
+    } catch (error) {
+        throw createError(error.message)
+    }
 })
 ]
 
